49c9e09749c06a7261fb374e51fb1fe9a6e638ba
MR <moayeut@gmail.com>
2024-09-07 14:15:13 +0200

    add calculator_mri

--- components/calculatorView.tsx
+++ components/calculatorView.tsx
@@ -2,8 +2,24 @@ import { IStep } from "@/model/instructions";
 import { IProject, Thread } from "@/model/project";
 import { Speech } from "@/tools/speech";
 import { CalculatorMessageType, ICalculatorMessage, ICalculatorProgress } from "@/workers/workers";
-import { Button, ButtonGroup, Checkbox, FormControlLabel, LinearProgress, Stack, Typography } from "@mui/material";
+import { Button, ButtonGroup, Checkbox, FormControl, FormControlLabel, InputLabel, LinearProgress, MenuItem, OutlinedInput, Select, Stack, Typography } from "@mui/material";
 import React from "react";
+
+type CalculatorInfo = {
+    label: string
+    key: string
+}
+
+const calculators: Array<CalculatorInfo> = [
+    {
+        label: 'Delta',
+        key: 'calculator'
+    },
+    {
+        label: 'MRI like',
+        key: 'calculator_mri'
+    }
+]
 
 interface IOptions {
     project: IProject
@@ -16,6 +32,7 @@ interface IState {
     isRunning: boolean
     progress: ICalculatorProgress
     error?: string
+    calculator: string
 }
 
 function preventClose() {
@@ -26,6 +43,7 @@ export default function ({ project, imageDatas, onChange }: IOptions) {
     const [state, setState] = React.useState<IState>({
         threads: [],
         isRunning: false,
+        calculator: calculators[1].key,
         progress: {
             stepIndex: 0,
             threadIndex: 0
@@ -37,6 +55,7 @@ export default function ({ project, imageDatas, onChange }: IOptions) {
         setState({
             threads: [],
             isRunning: false,
+            calculator: calculators[1].key,
             progress: {
                 stepIndex: 0,
                 threadIndex: 0
@@ -44,6 +63,7 @@ export default function ({ project, imageDatas, onChange }: IOptions) {
         })
         setDatas(project)
     }, [project])
+
 
     const end = ({ error, result }: { error?: string, result?: IStep[] }) => {
         window.onbeforeunload = null
@@ -60,7 +80,18 @@ export default function ({ project, imageDatas, onChange }: IOptions) {
     const start = async () => {
         if (state.isRunning) return
 
-        const worker = new Worker(new URL('../workers/calculator', import.meta.url))
+        var worker: Worker | undefined //= new Worker(new URL(`/workers/${state.calculator}`, import.meta.url))
+
+        switch (state.calculator) {
+            case 'calculator':
+                worker = new Worker(new URL('/workers/calculator', import.meta.url))
+                break;
+            case 'calculator_mri':
+                worker = new Worker(new URL('/workers/calculator_mri', import.meta.url))
+                break;
+        }
+
+        if (!worker) return
 
         worker.onmessage = (event: MessageEvent<ICalculatorMessage>) => {
             switch (event.data.type) {
@@ -82,7 +113,7 @@ export default function ({ project, imageDatas, onChange }: IOptions) {
 
         worker.onerror = (event: ErrorEvent) => {
             end({ error: event.error })
-            console.log('🍎 Unexpected error: ', event)
+            console.error('Unexpected error: ', event)
         }
 
         setDatas(project)
@@ -109,7 +140,30 @@ export default function ({ project, imageDatas, onChange }: IOptions) {
     return (
         <Stack >
             {!state.isRunning &&
-                <React.Fragment>
+                <Stack direction='row'>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                        <InputLabel id="select-label">Calculator</InputLabel>
+                        <Select
+                            labelId="select-label"
+                            id="select"
+                            value={state.calculator}
+                            onChange={(e) => setState({
+                                ...state,
+                                calculator: e.target.value
+                            })}
+                            label="Calculator"
+                        >
+                            {calculators.map((calculator) => (
+                                <MenuItem
+                                    key={calculator.key}
+                                    value={calculator.key}
+                                >
+                                    {calculator.label}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+
                     {project.threads.map((thread: Thread, index: number) => (
                         <FormControlLabel
                             key={`cb_${index}`}
@@ -119,10 +173,11 @@ export default function ({ project, imageDatas, onChange }: IOptions) {
                                     threads: project.threads.map((t: Thread, i: number) => index == i ? event.target.checked : i < state.threads.length ? state.threads[i] : true)
                                 })} />} label={thread.description} />
                     ))}
+
                     <ButtonGroup variant="outlined" sx={{ marginBottom: 2 }}>
                         <Button onClick={async () => start()}>Start</Button>
                     </ButtonGroup>
-                </React.Fragment>
+                </Stack>
             }
             {state.isRunning &&
                 <React.Fragment>
---
+++ workers/calculator_mri.tsx
@@ -0,0 +1,183 @@
+import { RotationDirection } from "@/enums/rotationDirection"
+import { IStep, IInstructions, INail } from "@/model/instructions"
+import { IProject, Thread } from "@/model/project"
+import { IPixelLine, IPixelLineEvaluation, IWeightPoint2D, PixelLine, PixelLineMode } from "@/tools/calculation/PixelLine"
+import { ILine2D, Line2D } from "@/tools/geometry/Line2D"
+import { IPoint2D } from "@/tools/geometry/Point2D"
+import { CalculatorMessageType, ICalculatorInput, ICalculatorMessage, ICalculatorProgress } from "./workers"
+
+interface ILineInfo extends ILine2D {
+    n0Idx: number
+    r0: RotationDirection
+    n1Idx: number
+    r1: RotationDirection
+}
+
+type WeightLine = {
+    line: ILineInfo
+    weight: number
+}
+
+function post(type: CalculatorMessageType, value: any) {
+    const msg: ICalculatorMessage = {
+        type,
+        value
+    }
+    postMessage(msg)
+}
+
+function postProgress(data: ICalculatorProgress) {
+    post(CalculatorMessageType.Progress, data)
+}
+
+function calculate(project: IProject, imageDatas: Array<Uint8ClampedArray>, threads: Array<boolean>): Array<IStep> {
+    const nails: Array<INail> = project.nailMap.nails
+
+    let minX: number | undefined,
+        maxX: number | undefined,
+        minY: number | undefined,
+        maxY: number | undefined
+
+    const updateMinMax = (p: IPoint2D) => {
+        minX = Math.min(minX ?? p.x, p.x)
+        maxX = Math.max(maxX ?? p.x, p.x)
+        minY = Math.min(minY ?? p.y, p.y)
+        maxY = Math.max(maxY ?? p.y, p.y)
+    }
+
+    const getLineInfo = (n0Idx: number, r0: RotationDirection, n1Idx: number, r1: RotationDirection): ILineInfo => {
+        const line: ILine2D = Line2D.getTangeant(nails[n0Idx].position, nails[n0Idx].diameter, r0, nails[n1Idx].position, nails[n1Idx].diameter, r1)
+        updateMinMax(line.p0)
+        updateMinMax(line.p1)
+        return { ...line, n0Idx, r0, n1Idx, r1 }
+    }
+
+    console.log(`worker: calculate tangeant lines`)
+    const lines: ILineInfo[][][] = project.nailMap.lines.map((lines: number[], n0Idx: number) => [
+        lines.map((n1Idx: number) => [
+            getLineInfo(n0Idx, RotationDirection.ClockWise, n1Idx, RotationDirection.ClockWise),
+            getLineInfo(n0Idx, RotationDirection.ClockWise, n1Idx, RotationDirection.AntiClockWise)
+        ]).reduce((a: ILineInfo[], b: ILineInfo[]) => a.concat(b), []),
+        lines.map((n1Idx: number) => [
+            getLineInfo(n0Idx, RotationDirection.AntiClockWise, n1Idx, RotationDirection.ClockWise),
+            getLineInfo(n0Idx, RotationDirection.AntiClockWise, n1Idx, RotationDirection.AntiClockWise)
+        ]).reduce((a: ILineInfo[], b: ILineInfo[]) => a.concat(b), []),
+    ])
+
+    minX = (minX! - project.nailMap.position.x) / project.nailMap.scale
+    maxX = (maxX! - project.nailMap.position.x) / project.nailMap.scale
+    minY = (minY! - project.nailMap.position.y) / project.nailMap.scale
+    maxY = (maxY! - project.nailMap.position.y) / project.nailMap.scale
+
+    console.log(`worker: calculate pixel lines`)
+
+    var result: Array<IStep> = []
+
+    for (let i = 0; i < project.threads.length; i++) {
+        const thread: Thread = project.threads[i]
+        const imageData: Uint8ClampedArray = imageDatas[i]
+        let stepCount: number = 0
+
+        if (i < threads.length && !threads[i]) {
+            console.log(`worker: thread "${thread.description}" ignored`)
+            continue
+        }
+
+        console.log(`worker: thread "${thread.description}" calculate target`)
+
+        const target: number[][] = Array
+            .from(Array(Math.floor(maxX! - minX! + 4)).keys())
+            .map((x: number) =>
+                Array.from(Array(Math.floor(maxY! - minY! + 4)).keys())
+                    .map((y: number) => {
+                        const ix = Math.floor(minX! + x)
+                        const iy = Math.floor(minY! + y)
+                        if (ix < 0 || ix >= thread.imageInfo.width) return 0
+                        if (iy < 0 || iy >= thread.imageInfo.height) return 0
+                        const idx: number = 4 * (iy * thread.imageInfo.width + ix)
+                        return 1 - (imageData[idx] + imageData[idx + 1] + imageData[idx + 2]) / 3 / 255
+                    }))
+
+        console.log(`target[${target.length}][${target[0].length}]`)
+
+        const linesWeights: WeightLine[][][] = lines.map((a0: ILineInfo[][]) =>
+            a0.map((a1: ILineInfo[]) =>
+                a1.map((l: ILineInfo) => {
+                    const pLine = PixelLine.get({
+                        x: (l.p0.x - project.nailMap.position.x) / project.nailMap.scale - minX! + 2,
+                        y: (l.p0.y - project.nailMap.position.y) / project.nailMap.scale - minY! + 2,
+                    }, {
+                        x: (l.p1.x - project.nailMap.position.x) / project.nailMap.scale - minX! + 2,
+                        y: (l.p1.y - project.nailMap.position.y) / project.nailMap.scale - minY! + 2,
+                    }, PixelLineMode.Bresenham)
+
+                    return {
+                        line: l,
+                        weight: pLine.points.reduce((sum: number, p: IWeightPoint2D) => sum + p.weight * target[p.x][p.y], 0)
+                    }
+                })
+            )
+        )
+
+        var nail: IStep | undefined = {
+            nailIndex: 0,
+            direction: RotationDirection.ClockWise
+        }
+
+        console.log(`worker: thread "${thread.description}" search path`)
+        var start = new Date()
+        do {
+            let nextNail: IStep | undefined
+            let indicator: number = 0
+
+            const arr = linesWeights[nail!.nailIndex][nail!.direction]
+
+            for (let j = 0; j < arr.length; j++) {
+
+                if (arr[j].weight <= indicator)
+                    continue
+
+                indicator = arr[j].weight
+                arr[j].weight = 0
+                nextNail = {
+                    nailIndex: arr[j].line.n1Idx,
+                    direction: arr[j].line.r1
+                }
+            }
+
+            if (nextNail) {
+                result.push(nextNail)
+                stepCount++
+            }
+
+            const end = new Date()
+            if (end.valueOf() - start.valueOf() > 500) {
+                postProgress({
+                    stepIndex: stepCount,
+                    threadIndex: i
+                })
+                start = end
+            }
+
+            nail = nextNail
+        }
+        while (nail && stepCount < thread.maxStep)
+
+        postProgress({
+            stepIndex: stepCount,
+            threadIndex: i
+        })
+    }
+
+    return result
+}
+
+self.onmessage = async (event: MessageEvent<ICalculatorInput>) => {
+    console.log('🐝 Worker: Message received from main script')
+    const data = event.data
+    const result = calculate(data.project, data.imageDatas, data.threads)
+    console.log('🐝 Worker: end', result)
+    post(CalculatorMessageType.Result, result)
+}
+
+export { }
